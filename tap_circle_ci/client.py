c1c7dc859bf788422dd6c9e1f5b667c3ffd8c42b
Fran Lozano <fjlozanos@gmail.com>
2021-12-26 20:48:03 +0100

    Add files

---
+++ tap_circle_ci/client.py
@@ -0,0 +1,68 @@
+"""REST client handling, including CircleCIStream base class."""
+
+import requests
+from pathlib import Path
+from typing import Any, Dict, Optional, Union, List, Iterable
+
+from memoization import cached
+
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.streams import RESTStream
+from singer_sdk.authenticators import APIKeyAuthenticator
+
+
+SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+
+
+class CircleCIStream(RESTStream):
+    """CircleCI stream class."""
+
+    records_jsonpath = '$.items[*]'
+    next_page_token_jsonpath = "$.next_page_token"  # Or override `get_next_page_token`.
+
+    @property
+    def url_base(self) -> str:
+        return self.config.get('base_url')
+
+    @property
+    def authenticator(self) -> APIKeyAuthenticator:
+        """Return a new authenticator object."""
+        return APIKeyAuthenticator.create_for_stream(
+            self,
+            key="Circle-Token",
+            value=self.config.get("token"),
+            location="header"
+        )
+
+    @property
+    def http_headers(self) -> dict:
+        """Return the http headers needed."""
+        headers = {}
+        if "user_agent" in self.config:
+            headers["User-Agent"] = self.config.get("user_agent")
+        # If not using an authenticator, you may also provide inline auth headers:
+        # headers["Private-Token"] = self.config.get("auth_token")
+        return headers
+
+    def get_next_page_token(
+        self, response: requests.Response, previous_token: Optional[Any]
+    ) -> Optional[Any]:
+        """Return a token for identifying next page or None if no more pages."""
+        all_matches = extract_jsonpath(
+            self.next_page_token_jsonpath, response.json()
+        )
+        return next(iter(all_matches), None)
+
+    def get_url_params(
+        self, context: Optional[dict], next_page_token: Optional[Any]
+    ) -> Dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization."""
+        params: dict = {}
+        if next_page_token:
+            params["page-token"] = next_page_token
+        return params
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result rows."""
+        yield from extract_jsonpath(self.records_jsonpath, input=response.json())
+
---
+++ tap_circle_ci/tap.py
@@ -0,0 +1,53 @@
+"""CircleCI tap class."""
+
+from typing import List
+
+from singer_sdk import Tap, Stream
+from singer_sdk import typing as th
+from singer_sdk.helpers._classproperty import classproperty
+
+from tap_circle_ci.streams import (
+    PipelinesStream,
+    WorkflowsStream,
+    JobsStream
+)
+
+STREAM_TYPES = [
+    PipelinesStream,
+    WorkflowsStream,
+    JobsStream
+]
+
+
+class TapCircleCI(Tap):
+    """Singer tap for the CircleCI API."""
+    name = "tap-circle-ci"
+
+    @classproperty
+    def config_jsonschema(cls):
+        return th.PropertiesList(
+            th.Property(
+                "token",
+                th.StringType,
+                required=True,
+                description="Personal API Token you have generated that can be used to access the CircleCI API"
+            ),
+            th.Property(
+                "org_slug",
+                th.StringType,
+                required=True,
+                description="Organization slug in the form vcs-slug/org-name. "
+                            "Example: org-slug=gh/CircleCI-Public"
+            ),
+            th.Property("base_url", th.StringType, default="https://circleci.com/api/v2"),
+            th.Property(
+                "user_agent",
+                th.StringType,
+                default=f"{cls.name}/{cls.plugin_version} {cls.__doc__}",
+                description="User-Agent header",
+            )
+        ).to_dict()
+
+    def discover_streams(self) -> List[Stream]:
+        """Return a list of discovered streams."""
+        return [stream_class(tap=self) for stream_class in STREAM_TYPES]
