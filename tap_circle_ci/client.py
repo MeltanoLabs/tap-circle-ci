ed45e1837b2bb291cda8aeb500f86cf2f3e8447d
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-10-02 13:22:39 -0600

    feat: Drop support for Python 3.7

--- tap_circle_ci/client.py
+++ tap_circle_ci/client.py
@@ -41,7 +41,7 @@ class CircleCIStream(RESTStream):
         return headers
 
     def get_url_params(
-        self,
+        self,  # noqa: PLR6301
         context: dict | None,  # noqa: ARG002
         next_page_token: str | None,
     ) -> dict[str, Any]:
