3f5c34577ea809203880ca88d275fe369078808a
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2024-02-29 21:55:03 -0600

    fix: Fixed the package name so the right version can be retrieved (#105)

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,11 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-from singer_sdk.testing import get_tap_test_class
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
 
 from tap_circle_ci.tap import TapCircleCI
 
-TestTapCircleCI = get_tap_test_class(TapCircleCI, config={})
+TestTapCircleCI = get_tap_test_class(
+    TapCircleCI,
+    config={},
+    suite_config=SuiteConfig(max_records_limit=25),
+)
